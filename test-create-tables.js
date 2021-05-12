7472499b02dd1f638d595b12ccf2cb4971b5938b
Luan Marques <luanmarques1789@gmail.com>
2021-05-12 12:57:42 -0300

    test pt. 1

--- test-create-tables.js
+++ test-create-tables.js
@@ -1,60 +1,17 @@
-const mysql = require('mysql2/promise'); // versão promissificada
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'san01xlz',
+  database: 'cat-products',
+  port: 3307,
+});
 
-const run = async () => {
-	try {
-		const conn = await mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: 'san01xlz',
-			database: 'cat-products',
-			port: 3307,
-		});
-		try {
-			const [results] = await conn.query(`SHOW TABLES`);
-			console.log(results);
-			if (results.length === 0) {
-				await conn.query(`
-          CREATE TABLE categories(
-            id INTEGER NOT NULL AUTO_INCREMENT,
-            category VARCHAR(255) NOT NULL,
-            PRIMARY KEY(id)
-          );
+connection.connect();
 
-          CREATE TABLE products(
-            id INTEGER NOT NULL AUTO_INCREMENT,
-            product VARCHAR(255) NOT NULL,
-            price FLOAT NOT NULL, 
-            PRIMARY KEY(id),
-            );
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
 
-          CREATE TABLE images(
-            id INTEGER NOT NULL AUTO_INCREMENT,
-            description VARCHAR(255) NOT NULL,
-            url NOT NULL,
-            product_id INT NOT NULL,
-            PRIMARY KEY(id),
-            
-            CONSTRAINT fk_images_products_constr 
-            FOREIGN KEY (product_id)
-            REFERENCES products(id)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE
-          );
-          `);
-
-				console.log('Tables were created with success');
-			} else {
-				console.log('Tables already exist');
-			}
-
-			conn.end();
-		} catch (err) {
-			console.log('Aqui está o erro:', err);
-			conn.end();
-		}
-	} catch (err) {
-		console.log(err);
-	}
-};
-
-run();
+connection.end();
